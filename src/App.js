e184d399a4c0e850129b56f1247d4ffeadee3c2f
motorcykle <mohamedabdulridha1@gmail.com>
2022-06-18 14:35:31 +0200

    added structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
+import { Routes, Route } from "react-router-dom";
+import Layout from './components/layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
 
 function App() {
   return (
-    <div>building this app</div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="services" element={<Services />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes> 
+    </Layout>
   );
 }
 
